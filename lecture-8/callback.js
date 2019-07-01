7d475fa9e14897721294272a1dedc94d3e2deec9
shobhu98 <46121289+shobhu98@users.noreply.github.com>
2019-07-01 13:32:04 +0530

    a

--- lecture-8/callback.js
+++ lecture-8/callback.js
@@ -1,29 +1,62 @@
+// function ImageProcessing() {
+//     setTimeout(function () {
+//         console.log("Reading Image 1");
+//         // Test();
+//         readFile(Test);
+//
+//     },0);
+//
+//     function Test(){
+//         setTimeout(function () {
+//             console.log("Reading Image 2");
+//         },0);
+//
+//     }
+//     console.log("Processing....");
+//
+// }
+//
+// function ReadFile() {
+//     console.log("File was Read");
+//
+// }
+//
+// function getData() {
+//     console.log("function got data")
+//
+// }
+// function readFile(test) {
+//     setTimeout(function () {
+//         // file Extract
+//         // file Test
+//         test();
+//     },0);
+//
+//
+// }
+// ImageProcessing();//ASYNC Function
+// ReadFile();
+// getData();
+ImageProcessing();
+
 function ImageProcessing() {
+    readFile("240*240",function (res) {
+        console.log("Processing Ended");
+        console.log(res);
+
+
+    });
+
+
+    console.log("Processing started");
+
+}
+function readFile(number,cb) {
     setTimeout(function () {
-        console.log("Reading Image 1");
-        Test();
+        console.log("Reading File");
+        cb(number);
 
     },0);
 
-    function Test(){
-        setTimeout(function () {
-            console.log("Reading Image 2");
-        },0);
-
-    }
-    console.log("Processing....");
 
 }
-
-function ReadFile() {
-    console.log("File was Read");
-
-}
-
-function getData() {
-    console.log("function got data")
-    
-}
-ImageProcessing();//ASYNC Function
-ReadFile();
-getData();
